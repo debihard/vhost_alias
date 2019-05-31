369a885729f10f3a69088061b0c2ba73a6735455
John <50121345+debihard@users.noreply.github.com>
2019-06-01 00:18:13 +0500

    Update vhost_alias.sh

--- vhost_alias.sh
+++ vhost_alias.sh
@@ -37,6 +37,8 @@ done
 cat /${menu[$option]}
 echo -n "Type your new domain name: "; read newdomainname
 sed "s/ServerAlias.*/& $newdomainname/" -i /etc/apache2/sites-available/${menu[$option]}.conf
+a2dissite ${menu[$option]}
+a2ensite ${menu[$option]}
 certbot --apache --register-unsafely-without-email;
 echo -e $"Complete!\nYou just add new alias  $newdomainname to Virtual Host config"
 exit 0;
